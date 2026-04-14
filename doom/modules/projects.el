f789d1a0b5c5900aa57d9e9ec8eb82e5743cb70a
bugsworld38 <vmchr344@gmail.com>
2026-04-14 21:00:58 +0300

    doom config changes

--- doom/modules/projects.el
+++ doom/modules/projects.el
@@ -1,6 +1,3 @@
 ;;; modules/projects.el -*- lexical-binding: t; -*-
 
-(setq projectile-project-search-path '("~/Workspace/personal/github.com/bugsworld38"
-                                       ("~/Workspace/work/my-fiege/client-applications" . 2)
-                                       "~/Workspace/work/my-fiege/shared"
-                                       "~/Workspace/work/my-fiege/legacy"))
+(setq projectile-project-search-path '("~/Workspace/personal/github.com/bugsworld38"))
